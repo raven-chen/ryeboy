f67cd55290b7ab04921abf9513e284fd7612b8b4
Raven <enn.raven@gmail.com>
2014-05-01 08:37:21 +0800

    Hide unnecessary information from user list.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -55,22 +55,19 @@ RailsAdmin.config do |config|
   config.model 'User' do
     hide_attributes = [:reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at,
                   :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :password,
-                  :password_confirmation, :roles_mask, :user_activities, :fund_exchange_activities, :fines, :my_tasks, :tasks, :exercises, :comments]
+                  :password_confirmation, :roles_mask, :user_activities, :fund_exchange_activities, :fines, :my_tasks, :tasks, :exercises, :comments,
+                  :created_at, :updated_at]
+
+    list do
+      hide_attributes.each {|attr| configure(attr) {hide} }
+    end
 
     edit do
-      hide_attributes.each do |attr|
-        configure(attr) do
-          hide
-        end
-      end
+      hide_attributes.each {|attr| configure(attr) {hide} }
     end
 
     show do
-      hide_attributes.each do |attr|
-        configure(attr) do
-          hide
-        end
-      end
+      hide_attributes.each {|attr| configure(attr) {hide} }
     end
   end
 
