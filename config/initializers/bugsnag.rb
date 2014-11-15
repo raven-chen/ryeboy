8893194e74d85592ae4284debc4b0e42637f9084
Raven <enn.raven@gmail.com>
2014-11-15 10:57:01 +0800

    Only notify bugsnag in production env.

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,3 +1,5 @@
-Bugsnag.configure do |config|
-  config.api_key = "188ba657ab8a98f2391f09d5ef66c29f"
+if Rails.env == "production"
+  Bugsnag.configure do |config|
+    config.api_key = "188ba657ab8a98f2391f09d5ef66c29f"
+  end
 end
