48ae952a75b17f4c89cf946798988b798c243cdd
Raven <enn.raven@gmail.com>
2015-02-07 19:53:13 +0800

    Simplify backup file name to reduce the difficulty of remote backup shell.

--- config/system/db_backup.sh
+++ config/system/db_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Backup tao_production db
-mysqldump -utao -pKiBr11EY23nj --sock=/tmp/mysql.sock tao_production | gzip > /home/app/backups/tao_`date +\%Y-\%m-\%d-\%T`.sql.gz
+mysqldump -utao -pKiBr11EY23nj --sock=/tmp/mysql.sock tao_production | gzip > /home/app/backups/tao_`date +\%Y-\%m-\%d`.sql.gz
 
 # Backup tao_1s_production db
-mysqldump -utao_1s -pcJvauLgtd0ul --sock=/tmp/mysql.sock tao_1s_production | gzip > /home/app/backups/tao_1s_`date +\%Y-\%m-\%d-\%T`.sql.gz
+mysqldump -utao_1s -pcJvauLgtd0ul --sock=/tmp/mysql.sock tao_1s_production | gzip > /home/app/backups/tao_1s_`date +\%Y-\%m-\%d`.sql.gz
