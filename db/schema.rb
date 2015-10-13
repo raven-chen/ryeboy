b7756c27ec13ac5d46904028853a52b2b9a9210f
Raven <enn.raven@gmail.com>
2015-10-14 07:59:58 +0800

    Update schema..

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151014093854) do
+ActiveRecord::Schema.define(:version => 20151013122944) do
 
   create_table "comments", :force => true do |t|
     t.integer  "author_id"
