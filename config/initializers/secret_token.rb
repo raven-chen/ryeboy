76de293a9029815ebf6c66e8403c5f6b9f729047
Raven <enn.raven@gmail.com>
2015-10-24 11:40:38 +0800

    Add secrets.yml

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Tao::Application.config.secret_token = 'c36c0b7e86cdb75f7f65cfefbf4c24caecf45b489a5098f182f5f2f87d0755b061bd8155b50d54ce42f02845e34a0d8ea6935664f69063a7a19c07bdf2a7eaf3'
