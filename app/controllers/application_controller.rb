e379e025c9b64493167b66f9cd1738c8d333ec62
Raven <enn.raven@gmail.com>
2015-10-30 18:33:05 +0800

    Fix cannot register bug.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_path, :alert => exception.message
@@ -17,5 +18,11 @@ class ApplicationController < ActionController::Base
     }
 
     options
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:email, :name, :password, :password_confirmation]
   end
 end
