a9a44e7d9a7437efa0fb203fc26caac4c9980472
Raven <enn.raven@gmail.com>
2015-11-14 10:47:53 +0800

    Update backup.sh

--- config/system/db_backup.sh
+++ config/system/db_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Backup tao_production db
-mysqldump -utao -pKiBr11EY23nj --sock=/tmp/mysql.sock tao_production | gzip > /home/app/backups/tao_`date +\%Y-\%m-\%d`.sql.gz
+mysqldump -uryeboy -pESqOeI22MWrk ryeboy_production | gzip > /home/app/backups/ryeboy_`date +\%Y-\%m-\%d`.sql.gz
 
 # Backup tao_1s_production db
-mysqldump -utao_1s -pcJvauLgtd0ul --sock=/tmp/mysql.sock tao_1s_production | gzip > /home/app/backups/tao_1s_`date +\%Y-\%m-\%d`.sql.gz
+mysqldump -ulotus -pQenzw6YwyLOq lotus_production | gzip > /home/app/backups/lotus_`date +\%Y-\%m-\%d`.sql.gz
