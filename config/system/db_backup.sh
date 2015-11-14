79f77bf0597b9848af77aea1a40d52de68b62d6b
Raven <enn.raven@gmail.com>
2015-11-14 11:09:35 +0800

    Only keep recent 1 month backups.

--- config/system/db_backup.sh
+++ config/system/db_backup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# Backup tao_production db
+# Backup ryeboy_production db
 mysqldump -uryeboy -pESqOeI22MWrk ryeboy_production | gzip > /home/app/backups/ryeboy_`date +\%Y-\%m-\%d`.sql.gz
 
-# Backup tao_1s_production db
+# Backup lotus_production db
 mysqldump -ulotus -pQenzw6YwyLOq lotus_production | gzip > /home/app/backups/lotus_`date +\%Y-\%m-\%d`.sql.gz
+
+# Keep recent 1 month backups
+find /home/app/backups/ -type f -ctime +30 -delete
