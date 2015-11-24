47b972c7920361857b3d8498be8337a1dd3f4ac0
Raven <enn.raven@gmail.com>
2015-11-24 17:57:12 +0800

    Rename newbie guide text.
    Remove self study link.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -19,11 +19,6 @@ module UsersHelper
     end
   end
 
-  def self_study_notice user
-    # TODO: move this to site configuration
-    content_tag(:div, link_to("自学流程", "http://bbs.ryeboy.org/topics/133", :class => "btn btn-success btn-large", :target => "_blank"), :class => "self-study alert alert-warning") if user.blank? || user.newbie?
-  end
-
   def author_info object
     content_tag(:span, "#{object.author.try(:name)} 创建于 #{I18n.l(object.created_at)}", :class => "hint-text author-info")
   end
