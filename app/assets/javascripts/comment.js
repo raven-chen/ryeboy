7d2049eaf7b38fc55fa20e5433199630b7d5c1e8
Raven <enn.raven@gmail.com>
2016-02-13 11:29:22 +0800

    Focus on editor after click on "reply" button.

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,6 +1,6 @@
 $(function(){
   $(".js-reply-to-comment a.js-reply").click(function(){
     $("#comment_replied_comment_id").val($(this).data("comment-id"));
-    $(".note-editable").attr("data-placeholder", $(this).data("reply-to"));
+    $(".note-editable").attr("data-placeholder", $(this).data("reply-to")).focus();
   });
 });
