dd7456b36186a7597e93805d0488666644673019
Raven <enn.raven@gmail.com>
2016-04-25 20:16:19 +0800

    Add index on comments.userid and createdAt

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -4,7 +4,11 @@ class Diary
   embeds_many :comments, class_name: "LcComment"
 
   field :userid, type: String
+  field :content, type: String
   field :userlevel, type: String
   field :date, type: Date
   field :createdAt, type: Time
+
+  index "comments.userid" => 1
+  index "comments.createdAt" => 1
 end
